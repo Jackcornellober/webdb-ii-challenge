55521a2441f1b950f736fd48211d788576a1fab2
jackcornellober <jackcornellober@gmail.com>
2019-07-16 16:10:58 -0700

    tuesday MVP

---
+++ migrations/20190716145047_create-cars-table.js
@@ -0,0 +1,35 @@
+exports.up = function(knex) {
+    // we make changes to the db schema
+    return knex.schema.createTable('cars', tbl => {
+      // add a primary key named id, integer, auto-increment
+      tbl.increments();
+  
+      // other columns
+      tbl.integer('VIN')
+        .unique()
+        .notNullable();
+      tbl.string('make', 128)
+      .notNullable();
+      tbl.string('model', 128)
+      .notNullable();
+      tbl
+        .string('name', 128)
+        .unique()
+        .notNullable();
+      tbl.decimal('mileage')
+        .notNullable();
+        tbl.string('transmissionType', 128);
+        tbl.string('titleStatus', 128);
+    });
+  };
+  
+  exports.down = function(knex) {
+    // we undo the changes to the db schema
+    return knex.schema.dropTableIfExists('cars');
+  };
+  
+  // npx knex init
+  // npx knex migrate:make create-cars-table
+  // npx knex migrate:up
+  
+  // npx knex migrate:down to undo one migration at a time
